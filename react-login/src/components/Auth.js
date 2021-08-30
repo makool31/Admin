3a92f48a0886da7f80d6ddba4e55b089a3404c04
makool31 <makool31@yahoo.com>
2021-08-30 20:56:01 +0800

    initial commit

--- react-login/src/components/Auth.js
+++ react-login/src/components/Auth.js
@@ -1,70 +1,54 @@
-import React,  { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 //import { useHistory } from 'react-router-dom';
-import LoginForm from './components/LoginForm';
+import LoginForm from "./components/LoginForm";
 
 //import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
-
-
 function Auth() {
-  
   const adminUser = {
     email: "admin@admin.com",
-    password: "admin123"
-  }
-  const[user, setUser] = useState({name: "", email: ""});
+    password: "admin123",
+  };
+  const [user, setUser] = useState({ name: "", email: "" });
   const [error, setError] = useState("");
 
-  const Login = details => {
+  const Login = (details) => {
     console.log(details);
 
-    if(details.email === adminUser.email && details.password === adminUser.password)
-    { 
+    if (
+      details.email === adminUser.email &&
+      details.password === adminUser.password
+    ) {
       console.log("Logged in");
       setUser({
         name: details.name,
-        
-        email: details.email
+
+        email: details.email,
       });
-    }
-    else
-    {
+    } else {
       console.log("Details do not match!");
       setError("Details do not match!");
-    } 
-   
-  }
-  const Logout = () =>
-  {
-    setUser({name: "", email: ""});
-  }
-
- 
+    }
+  };
+  const Logout = () => {
+    setUser({ name: "", email: "" });
+  };
 
   return (
     <div className="App">
-      
-      {(user.email !=="")? (
+      {user.email !== "" ? (
         <div>
-          <h2>Welcome, Admin <span>{user.name}</span></h2>
+          <h2>
+            Welcome, Admin <span>{user.name}</span>
+          </h2>
           <button onClick={Logout}>Logout</button>
-        </div> 
-      ):(
-        <LoginForm Login={Login} error={error}/>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
       )}
     </div>
-
-    
-  )
-
-      
-
-
-
-  }
+  );
+}
 
 export default Auth;
-
-
-
